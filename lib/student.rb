573923c892c44ee58473f2494f28a20daac67d5d
Gabriel Valle <gvalle91@gmail.com>
2018-06-28 19:54:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,6 +22,14 @@ class Student
         grade TEXT
       )
     SQL
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE IF EXISTS students
+    SQL
+    DB[:conn].execute(sql)
   end
 
 end
