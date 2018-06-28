ae22ff75caabf7f2a4c54f2d4176bc7908c2352e
Gabriel Valle <gvalle91@gmail.com>
2018-06-28 20:05:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ class Student
       UPDATE students SET name = ?, grade = ? WHERE id = ?
     SQL
     DB[:conn].execute(sql,self.name, self.grade, self.id)
-  end 
+  end
 
   def self.create(name, grade)
     new_student = self.new(name, grade)
@@ -41,6 +41,16 @@ class Student
 
   def self.new_from_db(array)
     self.new(array[1],array[2],arrary[0])
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE name = ?
+    SQL
+    DB[:conn].execute(sql,name).collect do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.create_table
